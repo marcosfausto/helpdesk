b5e321a59174a6b406d06ee98b8dadb476b8767e
Marcos <marcos_fausto@yahoo.com.br>
2022-12-11 11:25:54 +0000

    carga inicial no banco de dados

--- src/main/java/com/marcosfausto/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/marcosfausto/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,44 @@
 package com.marcosfausto.helpdesk;
 
+import com.marcosfausto.helpdesk.domain.Chamado;
+import com.marcosfausto.helpdesk.domain.Cliente;
+import com.marcosfausto.helpdesk.domain.Tecnico;
+import com.marcosfausto.helpdesk.domain.enums.Perfil;
+import com.marcosfausto.helpdesk.domain.enums.Prioridade;
+import com.marcosfausto.helpdesk.domain.enums.Status;
+import com.marcosfausto.helpdesk.repositories.ChamadoRepository;
+import com.marcosfausto.helpdesk.repositories.ClienteRepository;
+import com.marcosfausto.helpdesk.repositories.TecnicoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
+
+	private final TecnicoRepository tecnicoRepository;
+	private final ClienteRepository clienteRepository;
+	private final ChamadoRepository chamadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "55048215095", "valdir@mail.com", "123");
+		tec1.setPerfis(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "11166189074", "torvalds@mail.com", "123");
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+
+		tecnicoRepository.save(tec1);
+		clienteRepository.save(cli1);
+		chamadoRepository.save(c1);
+
+	}
 }
