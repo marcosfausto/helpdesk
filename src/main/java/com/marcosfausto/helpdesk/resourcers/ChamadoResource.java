a1f67082e1745410b29ef9b14107d5ff132360b0
Marcos <marcos_fausto@yahoo.com.br>
2022-12-19 21:10:11 +0000

    findAll para chamado

--- src/main/java/com/marcosfausto/helpdesk/resourcers/ChamadoResource.java
+++ src/main/java/com/marcosfausto/helpdesk/resourcers/ChamadoResource.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/chamados")
@@ -19,6 +22,11 @@ public class ChamadoResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(new ChamadoDTO(chamadoService.findById(id)));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        return ResponseEntity.ok().body(chamadoService.findAll().stream().map(ChamadoDTO::new).collect(Collectors.toList()));
     }
     
 }
--- src/main/java/com/marcosfausto/helpdesk/services/ChamadoService.java
+++ src/main/java/com/marcosfausto/helpdesk/services/ChamadoService.java
@@ -2,10 +2,15 @@ package com.marcosfausto.helpdesk.services;
 
 import com.marcosfausto.helpdesk.domain.Chamado;
 import com.marcosfausto.helpdesk.domain.Cliente;
+import com.marcosfausto.helpdesk.domain.dtos.ChamadoDTO;
 import com.marcosfausto.helpdesk.repositories.ChamadoRepository;
 import com.marcosfausto.helpdesk.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +20,9 @@ public class ChamadoService {
 
     public Chamado findById(Integer id) {
         return chamadoRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Chamado> findAll() {
+        return chamadoRepository.findAll();
     }
 }
